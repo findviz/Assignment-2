cd7bcac30c9cbe8609c38beb6741094161a5dbc1
Vishnu Mohan P <vm07@users.noreply.github.com>
2023-05-24 15:11:20 +0530

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,13 @@
+let mark = 75;
+if (mark > 90) {
+    console.log("Grade A");
+} 
+else if(mark > 70){
+    console.log("Grade B");
+}
+else if(mark > 50){
+    console.log("Grade C");
+}
+else if(mark > 30){
+    console.log("Grade D");
+}
---
+++ multiplicationtable.js
@@ -0,0 +1,6 @@
+
+let num = 6;
+console.log("Multiplication table of","",num)
+for(let i=0 ; i<=10 ; i++){
+    console.log(num+ "*" + i + " = " + num * i);
+}
